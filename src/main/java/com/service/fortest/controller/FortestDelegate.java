e3285d3e41b8d5b5496ee276fa59fd5beed8e63b
paas <paas@cse-codegen-backend-1893298629-lqqtb>
2018-04-11 12:09:27 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/fortest/controller/FortestDelegate.java
@@ -0,0 +1,14 @@
+package com.service.fortest.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FortestDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}

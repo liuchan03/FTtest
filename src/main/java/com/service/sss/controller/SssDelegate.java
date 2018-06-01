d905c97d9ed05daadb799f1bcb4a00a8f42d49ce
paas <paas@cse-codegen-backend-3955833172-zt95k>
2018-06-01 11:42:39 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/sss/controller/SssDelegate.java
@@ -0,0 +1,14 @@
+package com.service.sss.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SssDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
